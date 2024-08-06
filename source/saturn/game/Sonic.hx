b280fbc8c9512a0a4ac492ba1c8891a2d574eb21
Phlox <103864356+ue4pak@users.noreply.github.com>
2024-08-05 22:39:58 -0400

    added more variables

--- source/saturn/game/Sonic.hx
+++ source/saturn/game/Sonic.hx
@@ -8,7 +8,23 @@ class Sonic extends Sprites
 {
     private var sonic2ver:Bool = false;
     private var isSpindashing:Bool = false;
-    private var jump_force:Float = 6.5; // Thanks Sonic Physics Guide.
+    // Thanks Sonic Physics Guide.
+    // Basic Movement
+    private var jump_force:Float = 6.5;
+    private var acceleration_speed:Float = 0.046875; // (12 subpixels)
+    private var deceleration_speed:Float = 0.5; // (128 subpixels)
+    private var friction_speed:Float = 0.046875; // (12 subpixels)
+    private var top_speed:Float = 6;
+    private var gravity_force:Float = 0.21875; // (56 subpixels)
+    // Slope Factor
+    private var slope_factor_normal:Float = 0.125; // (32 subpixels)
+    private var slope_factor_rollup:Float =	0.078125; // (20 subpixels)
+    private var slope_factor_rolldown:Float = 0.3125; // (80 subpixels)
+    // Rolling
+    var roll_friction_speed:Float =	0.0234375; // (6 subpixels)
+    var roll_deceleration_speed:Float =	0.125; // (32 subpixels)
+    // Air State
+    var air_acceleration_speed:Float = 0.09375; // (24 subpixels)
 
     public function new(xPos:Int = 0, yPos:Int = 0) {
         super(xPos, yPos);
